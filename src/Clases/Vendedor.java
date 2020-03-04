74c363419c7fbaf7ee7456805c33421449d5aeb9
raulpenate <raul.h.penate@gmail.com>
2020-03-03 20:22:18 -0600

    Añadiendo clases y primera interfaz

---
+++ src/Clases/Vendedor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author raulh
+ */
+public class Vendedor {
+    public String código;
+    public String nombre;
+    public String dirección;
+    public String contacto;
+}
